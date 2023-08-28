2cd867a6bf824ec7f18c6dd926f3f527a8d82a91
gabrielagi <gabrielagi503@gmail.com>
2023-08-28 13:07:59 -0300

    Fixed return typeHandler

--- api/src/controllers/typeController.js
+++ api/src/controllers/typeController.js
@@ -4,16 +4,15 @@ const API_TYPE_URL = "https://pokeapi.co/api/v2/type";
 
 const getTypes = async () => {
   try {
-    const typesFromDb = await Type.findAll();
+    const typesFromDb = await Type.findAll({ raw: true });
 
     if (typesFromDb.length) {
       return typesFromDb;
     } else {
       const response = await axios.get(API_TYPE_URL);
       const types = response.data.results;
-
       await Type.bulkCreate(types);
-      return await Type.findAll();
+      return await Type.findAll({ raw: true });
     }
   } catch (error) {
     throw error;
--- api/src/handlers/typeHandlers.js
+++ api/src/handlers/typeHandlers.js
@@ -3,6 +3,7 @@ const { getTypes } = require("../controllers/typeController");
 const getTypesHandler = async (req, res) => {
   try {
     const types = await getTypes();
+    console.log("types en handler", types);
     res.status(200).json(types);
   } catch (error) {
     res.status(400).json({ error: error.message });
