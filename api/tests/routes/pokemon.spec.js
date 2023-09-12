9c0d74ed20d590a6ab120572c22497e5b57e3ea2
gabrielagi <gabrielagi503@gmail.com>
2023-09-12 11:35:09 -0300

    Jest + supertest + 1st Routes test

--- api/tests/routes/pokemon.spec.js
+++ api/tests/routes/pokemon.spec.js
@@ -1,24 +1,37 @@
-/* eslint-disable import/no-extraneous-dependencies */
-const { expect } = require('chai');
-const session = require('supertest-session');
-const app = require('../../src/app.js');
-const { Pokemon, conn } = require('../../src/db.js');
+// /* eslint-disable import/no-extraneous-dependencies */
+// const { expect } = require('chai');
+// const session = require('supertest-session');
+// const app = require('../../src/app.js');
+// const { Pokemon, conn } = require('../../src/db.js');
 
-const agent = session(app);
-const pokemon = {
-  name: 'Pikachu',
-};
+// const agent = session(app);
+// const pokemon = {
+//   name: 'Pikachu',
+// };
 
-describe('Pokemon routes', () => {
-  before(() => conn.authenticate()
-  .catch((err) => {
-    console.error('Unable to connect to the database:', err);
-  }));
-  beforeEach(() => Pokemon.sync({ force: true })
-    .then(() => Pokemon.create(pokemon)));
-  describe('GET /pokemons', () => {
-    it('should get 200', () =>
-      agent.get('/pokemons').expect(200)
-    );
+// describe('Pokemon routes', () => {
+//   before(() => conn.authenticate()
+//   .catch((err) => {
+//     console.error('Unable to connect to the database:', err);
+//   }));
+//   beforeEach(() => Pokemon.sync({ force: true })
+//     .then(() => Pokemon.create(pokemon)));
+//   describe('GET /pokemons', () => {
+//     it('should get 200', () =>
+//       agent.get('/pokemons').expect(200)
+//     );
+//   });
+// });
+
+// const { describe } = require('node:test')
+const app = require("../../src/app");
+const request = require("supertest");
+const agent = request(app);
+
+describe("Pokemon routes", () => {
+  describe("GET /pokemons/:id", () => {
+    it("Response with status: 200", async () => {
+      await agent.get("/pokemons/1").expect(200);
+    });
   });
 });
