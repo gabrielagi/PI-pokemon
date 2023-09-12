b1a87d8704a2a6724902776ed3286821111f2a6c
gabrielagi <gabrielagi503@gmail.com>
2023-09-12 14:35:30 -0300

    Test Routes | 1rs: Evaluate to create a New Pokemon in the Model + 2nd: Not allow duplicate names

--- api/tests/models/pokemon.spec.js
+++ api/tests/models/pokemon.spec.js
@@ -20,3 +20,55 @@
 //     });
 //   });
 // });
+
+const { expect } = require("chai");
+const db = require("../../src/db");
+
+describe("Pokemon Model", () => {
+  // Se sincroniza el modelo con la base de datos
+  beforeAll(async () => {
+    await db.conn.sync({ force: true });
+  });
+
+  it("should create a new Pokemon", async () => {
+    // Creo un nuevo Pokemon en la base de datos
+    const pokemon = await db.Pokemon.create({
+      name: "Pikachu",
+      img: "pokemonIMG",
+      hp: 100,
+      attack: 50,
+      defense: 40,
+    });
+
+    // Verifico que el Pokemon se haya creado correctamente
+    expect(pokemon.id).to.exist;
+    expect(pokemon.name).to.equal("Pikachu");
+    expect(pokemon.img).to.equal("pokemonIMG");
+    expect(pokemon.hp).to.equal(100);
+    expect(pokemon.attack).to.equal(50);
+    expect(pokemon.defense).to.equal(40);
+  });
+
+  it("should not allow duplicate names", async () => {
+    // Intento crear un Pokemon con el mismo nombre que uno existente
+    try {
+      await db.Pokemon.create({
+        name: "Pikachu",
+        img: "pokemonURL",
+        hp: 90,
+        attack: 60,
+        defense: 30,
+      });
+      // Si el Pokemon se crea con éxito la prueba falla
+      fail("Creating duplicate Pokemon should throw an error");
+    } catch (error) {
+      // Controlo que se haya lanzado un error de validación
+      expect(error.name).to.equal("SequelizeUniqueConstraintError");
+    }
+  });
+
+  // Esta función debería cierra la conexión con la base de datos
+  afterAll(async () => {
+    await db.conn.close();
+  });
+});
